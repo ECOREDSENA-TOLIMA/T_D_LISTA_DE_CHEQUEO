9238f821d0e529d95c1bb6e59acd907d400ac128
Sebastian Trujillo <129899570+strujilloa@users.noreply.github.com>
2023-12-19 10:01:22 -0500

    Update Lista de chequeo

--- src/config/global.js
+++ src/config/global.js
@@ -1,7 +1,8 @@
 export default {
   global: {
-    componenteFormativo: 'NOMBRE DEL COMPONENTE FORMATIVO',
-    descripcionCurso: 'BREVE DESCRIPCIÓN',
+    componenteFormativo: 'Lista de chequeo del proyecto',
+    descripcionCurso:
+      'Una lista de chequeo es un documento organizado que enumera elementos esenciales para realizar una tarea o evaluar un proceso. Facilita el seguimiento y la verificación de pasos, garantizando que se completen adecuadamente. En este documento contiene los elementos básicos de una lista de chequeo y la manera como se deben realizar, teniendo en cuenta el paso a paso y un ejemplo que se puede adaptar según las necesidades.',
     imagenBannerPrincipal: require('@/assets/curso/portada/banner-princiapal.svg'),
     fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.png'),
     imagenesDecorativasBanner: [
@@ -13,6 +14,10 @@ export default {
         clases: ['banner-principal-decorativo-2'],
         imagen: require('@/assets/curso/portada/banner-principal-decorativo-2.svg'),
       },
+      {
+        clases: ['banner-principal-decorativo-3'],
+        imagen: require('@/assets/curso/portada/banner-principal-decorativo-3.svg'),
+      },
     ],
   },
   menuPrincipal: {
@@ -22,62 +27,89 @@ export default {
         icono: 'fas fa-home',
         titulo: 'Volver al inicio',
       },
-      {
-        nombreRuta: 'introduccion',
-        icono: 'fas fa-info-circle',
-        titulo: 'Introducción',
-        desarrolloContenidos: true,
-      },
+      // {
+      //   nombreRuta: 'introduccion',
+      //   icono: 'fas fa-info-circle',
+      //   titulo: 'Introducción',
+      //   desarrolloContenidos: true,
+      // },
       {
         nombreRuta: 'tema1',
         numero: '1',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Lista de chequeo del proyecto',
         desarrolloContenidos: true,
         subMenu: [
           {
             numero: '1.1',
-            titulo: 'Titulo de segundo nivel',
+            titulo: 'Definición',
             hash: 't_1_1',
+          },
+          {
+            numero: '1.2',
+            titulo: 'Elementos que componen la lista de chequeo del proyecto',
+            hash: 't_1_2',
+          },
+          {
+            numero: '1.3',
+            titulo: 'Pasos para elaborar la evaluación del proyecto',
+            hash: 't_1_3',
+          },
+          {
+            numero: '1.4',
+            titulo:
+              'Recomendaciones para desarrollar la lista de chequeo del proyecto',
+            hash: 't_1_4',
+          },
+          {
+            numero: '1.5',
+            titulo:
+              'Herramientas digitales se pueden utilizar para la lista de chequeo del proyecto',
+            hash: 't_1_5',
+          },
+          {
+            numero: '1.6',
+            titulo: 'Ejemplo de la evaluación del proyecto',
+            hash: 't_1_6',
           },
         ],
       },
 
-      {
-        nombreRuta: 'tema2',
-        numero: '2',
-        titulo: 'Titulo de primer nivel',
-        desarrolloContenidos: true,
-      },
-      {
-        nombreRuta: 'tema3',
-        numero: '3',
-        titulo: 'Titulo de primer nivel',
-        desarrolloContenidos: true,
-      },
+      // {
+      //   nombreRuta: 'tema2',
+      //   numero: '2',
+      //   titulo: 'Titulo de primer nivel',
+      //   desarrolloContenidos: true,
+      // },
+      // {
+      //   nombreRuta: 'tema3',
+      //   numero: '3',
+      //   titulo: 'Titulo de primer nivel',
+      //   desarrolloContenidos: true,
+      // },
     ],
     subMenu: [
-      {
-        icono: 'fas fa-sitemap',
-        titulo: 'Síntesis',
-        nombreRuta: 'sintesis',
-        desarrolloContenidos: true,
-      },
-      {
-        nombreRuta: 'actividad',
-        icono: 'far fa-question-circle',
-        titulo: 'Actividad didáctica',
-        desarrolloContenidos: true,
-      },
-      {
-        nombreRuta: 'glosario',
-        icono: 'fas fa-sort-alpha-down',
-        titulo: 'Glosario',
-      },
-      {
-        nombreRuta: 'complementario',
-        icono: 'far fa-folder-open',
-        titulo: 'Material complementario',
-      },
+      // {
+      //   icono: 'fas fa-sitemap',
+      //   titulo: 'Síntesis',
+      //   nombreRuta: 'sintesis',
+      //   desarrolloContenidos: true,
+      // },
+      // {
+      //   nombreRuta: 'actividad',
+      //   icono: 'far fa-question-circle',
+      //   titulo: 'Actividad didáctica',
+      //   desarrolloContenidos: true,
+      // },
+      // {
+      //   nombreRuta: 'glosario',
+      //   icono: 'fas fa-sort-alpha-down',
+      //   titulo: 'Glosario',
+      // },
+      // {
+      //   nombreRuta: 'complementario',
+      //   icono: 'far fa-folder-open',
+      //   titulo: 'Material complementario',
+      // },
       {
         icono: 'fas fa-book',
         titulo: 'Referencias bibliográficas',
@@ -121,8 +153,35 @@ export default {
   ],
   referencias: [
     {
-      referencia: '',
-      link: '',
+      referencia:
+        'Cardona, C. y Restrepo, A. (s.f). Lista de Chequeo. Herramientas de control. Puntos de encuentro.',
+      link:
+        'http://puntosdeencuentro.weebly.com/uploads/2/2/3/6/22361874/listas_de_chequeo.pdf',
+    },
+    {
+      referencia:
+        'Mancera, M. (2012). Seguridad e higiene industria. Alfaomega.',
+      link:
+        'https://www.academia.edu/36319515/Seguridad_e_higiene_industrial_Mancera_FernA_ndez_Mario_Alfaomega_Colombia_pdf_1_1_1_',
+    },
+    {
+      referencia: 'Mancera, M. (2008). Lista de chequeo.',
+      link: 'http://www.manceras.com.co/publicaciones.htm',
+    },
+    {
+      referencia:
+        'Oliva, P. (2009). Listas de chequeo como técnica de control.',
+      link: 'https://claudiabernazza.com.ar/ssgp/html/pdf/check_list.pdf',
+    },
+    {
+      referencia:
+        'Organización Internacional de Normalización. (2015). ISO 9001:2015. Sistemas de gestión de la calidad.',
+      link: 'https://www.iso.org/obp/ui/#iso:std:iso:9001:ed-5:v1:es',
+    },
+    {
+      referencia:
+        'PDCA. (s.f). Check list / Listas de chequeo: ¿Qué es un checklist y cómo usarlo?',
+      link: 'http://www.pdcahome.com/check-list/',
     },
   ],
   creditos: [
--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -5,7 +5,123 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 1
-      h1 Titulo de primer nivel
+      h1 Lista de chequeo del proyecto
+    
+    .titulo-segundo.color-acento-contenido
+      h2#t_1_1 1.1 Definición
+    
+    p(data-aso="fade-left") Las listas de chequeo o de verificación es una técnica común que ayuda a controlar la ejecución de una actividad de manera rigurosa, además permite identificar resultados con base en variables relacionadas a la calidad de la ejecución, el cumplimiento, los riesgos o los factores que el líder del proyecto quiera medir.
+    .row.justify-content-center.align-items-center
+      .col-lg-5.col-8.mb-3
+        figure(data-aso="fade-right")
+          img(src='@/assets/curso/temas/tema1/1.jpg', alt='Imagen decorativa')
+      .col-lg-7.col-12.mb-3
+        p(data-aso="fade-left") Son formatos creados para realizar actividades repetitivas, controlar el cumplimiento de una lista de requisitos o recolectar datos de forma ordenada y sistemática. Se utilizan para realizar controles estandarizados de actividades o productos, asegurándose de no olvidar ningún punto importante.
+        .cajon.color-primario.p-4.mb-3(style="background-color: #CDF5E2")
+          p(data-aso="fade-left") Según Cardona y Restrepo (s.f.) la lista de chequeo es en sí misma, una ayuda para la memoria, al proporcionar un método para una rápida verificación de los ítems planteados según el tipo de asunto a controlar. Contribuyen a normalizar o estandarizar líneas de acción sistemáticas detallando cada uno de los puntos de actividad o proceso.
+    p(data-aso="fade-left") Con el seguimiento de una actividad a través de una lista de chequeo, se puede obtener la información para tomar medidas correctivas que ayuden a reducir el riesgo de fracaso del proyecto.
+    p(data-aso="fade-left") Por ejemplo, Mario Mancera (2012), asesor en higiene y seguridad industrial, explica que la lista de chequeo es uno de los mecanismos que ayudan a controlar los riesgos y que permiten detectar condiciones que representen peligro, que, para su campo profesional, se refiere a controlar las variables del ambiente y los cambios que puedan ocasionar peligro en la salud del hombre.
+
+    separador 
+    .titulo-segundo.color-acento-contenido
+      h2#t_1_2 1.2 Elementos que componen la lista de chequeo del proyecto
+    
+    p(data-aso="fade-left") La lista de chequeo, como herramienta metodológica está compuesta por una serie de ítems, factores, propiedades, aspectos, componentes, criterios, dimensiones o comportamientos, necesarios de tomarse en cuenta, para realizar una tarea, controlar y evaluar detalladamente el desarrollo de un proyecto, evento, producto o actividad. Dichos componentes se organizan de manera coherente para permitir que se evalúe de manera efectiva, la presencia o ausencia de los elementos individuales enumerados o por porcentaje de cumplimiento u ocurrencia. (Oliva, 2009). Algunos de los elementos son:
+
+    .row.justify-content-center
+      .col-lg-10.col-12 
+        .titulo-sexto.color-acento-contenido
+          h5 Figura 1.
+          span #[i Elementos de la lista de chequeo]
+        .col-12 
+          figure.desktop(data-aos="fade-right")
+            img(src='@/assets/curso/temas/tema1/2.svg' alt='En la figura 1 se muestran los diferentes elementos que componen una lista de chequeo.')
+        .row.justify-content-center
+          .col-12 
+            figure.movil(data-aos="fade-right")
+              img(src='@/assets/curso/temas/tema1/3.svg' alt='En la figura 1 se muestran los diferentes elementos que componen una lista de chequeo.')
+
+    separador 
+    .titulo-segundo.color-acento-contenido
+      h2#t_1_3 1.3 Pasos para elaborar la evaluación del proyecto
+    p(data-aos="fade-left") Para la elaboración de una lista de chequeo la norma ISO 9001 recomienda los siguientes pasos:
+
+    PasosA.color-acento-contenido.mb-5(tipo="l")
+      div
+        h3 Determinar el área que se quiere evaluar
+        p Los integrantes deben enfocar su atención hacia el análisis de las características del proceso, además deben tener claras las partes del proceso a observar para no perderse. Esta determinación es muy importante cuando una compañía cuenta con varias sedes o localizaciones.
+
+      div
+        h3 Diseñar el formato de verificación
+        p Escribir las categorías o variables posibles, establecer la escala de ocurrencias y diseñar la cuadrícula. Asegurarse de que todas las partes de la lista de chequeo estén claramente descritas. Esta herramienta busca minimizar problemas y ofrecer un formato claro y fácil de usar. Así que no hacen falta florituras ni colorines, mientras más básico, mejor.
+
+      div
+        h3 Tomar nota de la información en el formato de la verificación
+        p Esto puede variar de horas a semanas. Además, deben ser períodos realistas y que de verdad aporten información.
+
+      div
+        h3 Registrarlo en una base de datos para su tratamiento estadístico y análisis de los resultados
+        p Debe estarse seguro de que se dedique el tiempo necesario para esta actividad. El encargado o encargada de recopilar los datos, debe tomarse su tiempo, a veces hay procesos que son un poco repetitivos y tediosos y pueden llegar a cansar.
+
+    separador 
+    .titulo-segundo.color-acento-contenido
+      h2#t_1_4 1.4 Recomendaciones para desarrollar la lista de chequeo del proyecto
+    
+    .bloque-texto-g.color-primario.p-3.p-sm-4.p-md-5.mb-5
+      .bloque-texto-g__img(
+        :style="{'background-image': `url(${require('@/assets/curso/temas/tema1/4.jpg')})`}"
+      )
+      .bloque-texto-g__texto.p-4
+        ul.lista-ul
+          li 
+            i.fas.fa-angle-right
+            | La lectura del componente formativo, los enlaces relacionados en este, el material complementario, la revisión de los anexos, la guía de aprendizaje, así como el seguir de manera precisa las instrucciones de las actividades y orientaciones del instructor, son muy importantes para un logro eficaz de los resultados esperados en el proyecto formativo.
+          br
+          br
+          | 
+
+    separador 
+    .titulo-segundo.color-acento-contenido
+      h2#t_1_5 1.5 Herramientas digitales se pueden utilizar para la lista de chequeo del proyecto
+    
+    p(data-aos="fade-left") <b>El instructor le indicará la herramienta digital donde se desarrollará el debate asincrónico, no obstante, se listan a continuación algunas de ellas:</b> 
+
+    .row.justify-content-center.mt-5
+      .col-lg-10.col-12
+        .tarjeta--container.row.mb-5
+          .col-md.tarjeta.p-5(style="background-color: #CDF5E2")
+            .row.justify-content-center.mb-4
+              .col-6
+                figure
+                  img(src='@/assets/curso/temas/tema1/5.svg', alt='Texto que describa la imagen')
+            
+            h2.text-center <i>Word</i>
+            p Es el procesador de texto del paquete <i>Office</i> de <i>Microsoft</i>, es el más usado y tal vez potente en el medio, puede usarse como aplicación nativa o en línea.
+          .col-md.tarjeta.p-5(style="background-color: #DBE5F3")
+            .row.justify-content-center.mb-4
+              .col-6
+                figure
+                  img(src='@/assets/curso/temas/tema1/6.svg', alt='Texto que describa la imagen')
+            
+            h2.text-center <i>Excel</i>
+            p <i>Microsoft</i> <i>Office</i> <i>Excel</i> es una hoja de cálculo que sirve para manejar datos numéricos o alfanuméricos agrupados en filas y columnas también llamadas tablas de datos. Con la ayuda del editor VBA de <i>Excel</i> se pueden programar macros.
+
+    separador 
+    .titulo-segundo.color-acento-contenido
+      h2#t_1_6 1.6 Ejemplo de la evaluación del proyecto
+    
+    .row.justify-content-center
+      .col-lg-10.col-12 
+        .titulo-sexto.color-acento-contenido
+          h5 Tabla 1.
+          span #[i Ejemplo de lista de chequeo]
+        .col-12 
+          figure.desktop(data-aos="fade-right")
+            img(src='@/assets/curso/temas/tema1/7.svg' alt='En la figura 1 se muestran los diferentes elementos que componen una lista de chequeo.')
+          .col-sm-auto
+            a.boton.color-acento-botones.texto-blanco.movil(:href="obtenerLink('/downloads/anexo_Lista_de_chequeo.pdf')" target="_blank")
+              span Descargar
+              i.fas.fa-file-download
       
 </template>
 
